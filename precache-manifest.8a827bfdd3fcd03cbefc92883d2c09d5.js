59315a74f4cf0b247cc8a14be7fe1a9b07efd07d
jodigo <jodigo@jodigos-MacBook-Pro.local>
2020-06-27 23:23:46 -0700

    Updates

---
+++ precache-manifest.8a827bfdd3fcd03cbefc92883d2c09d5.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce946b337d017b947044e7bc7c8f9983",
+    "url": "/jihnius/index.html"
+  },
+  {
+    "revision": "756241e955def48e65c3",
+    "url": "/jihnius/static/css/main.88fbd28a.chunk.css"
+  },
+  {
+    "revision": "4dc57fbc52fbded349b0",
+    "url": "/jihnius/static/js/2.93bc2b28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jihnius/static/js/2.93bc2b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756241e955def48e65c3",
+    "url": "/jihnius/static/js/main.ee1a8f74.chunk.js"
+  },
+  {
+    "revision": "b67f278de4bb104cd49d",
+    "url": "/jihnius/static/js/runtime-main.6dae654d.js"
+  },
+  {
+    "revision": "d6a29860c93799da922a33368c44699a",
+    "url": "/jihnius/static/media/GameJam_Countdown1.d6a29860.jpg"
+  },
+  {
+    "revision": "8041d3be1c1a5a5d2c290baf043141a0",
+    "url": "/jihnius/static/media/GameJam_Countdown2.8041d3be.jpg"
+  },
+  {
+    "revision": "c79b3e542ba4db726aa700a96cd6a44c",
+    "url": "/jihnius/static/media/GameJam_Countdown3.c79b3e54.jpg"
+  },
+  {
+    "revision": "496d2aeda1fee03bd994fadbe458ebb0",
+    "url": "/jihnius/static/media/GameJam_Countdown4.496d2aed.jpg"
+  },
+  {
+    "revision": "07dfe72b3fdc266d771b783b8f448a26",
+    "url": "/jihnius/static/media/GameJam_Photo1.07dfe72b.jpg"
+  },
+  {
+    "revision": "f677102fe8a6986dd6bc2c23f8263b43",
+    "url": "/jihnius/static/media/GameJam_Photo2.f677102f.jpg"
+  },
+  {
+    "revision": "dcc3679e23d484669c6e5d96933df2d1",
+    "url": "/jihnius/static/media/GameJam_Photo3.dcc3679e.jpg"
+  },
+  {
+    "revision": "c8a08e36204b9ec87eb0b403c7a04a1f",
+    "url": "/jihnius/static/media/GameJam_Photo4.c8a08e36.jpg"
+  },
+  {
+    "revision": "c2e970067d90a83555059a48804c6cf7",
+    "url": "/jihnius/static/media/GameJam_Photo5.c2e97006.jpg"
+  },
+  {
+    "revision": "849d926abca75e4c22b17d85086f6215",
+    "url": "/jihnius/static/media/GameJam_Photo6.849d926a.jpg"
+  },
+  {
+    "revision": "dfc0259f46510f969bdbb39526c0d117",
+    "url": "/jihnius/static/media/GameJam_Photo7.dfc0259f.jpg"
+  },
+  {
+    "revision": "46809126d85a4bbbbe7a7ce2b4d00c45",
+    "url": "/jihnius/static/media/GameJam_Photo8.46809126.jpg"
+  },
+  {
+    "revision": "7fc496efebc687be00fa9e1afd7b5290",
+    "url": "/jihnius/static/media/GameJam_Poster.7fc496ef.jpg"
+  },
+  {
+    "revision": "3bec8b736910450578a2d7d726e4350a",
+    "url": "/jihnius/static/media/Jih-Yang Tan_Resume.3bec8b73.pdf"
+  },
+  {
+    "revision": "7b34c5aabcaa605e4f2c48afb43f9aa2",
+    "url": "/jihnius/static/media/Logo_Eastside.7b34c5aa.png"
+  },
+  {
+    "revision": "8a93daffa4bde7a6002d5975f0396c9d",
+    "url": "/jihnius/static/media/Logo_Hellbent.8a93daff.png"
+  },
+  {
+    "revision": "e0755859c342197632bc97bbf879c225",
+    "url": "/jihnius/static/media/Logo_Raccoopackstudios.e0755859.jpg"
+  },
+  {
+    "revision": "2397afff1175caa728662f6e1136e8f0",
+    "url": "/jihnius/static/media/Poster_Basketball.2397afff.jpg"
+  },
+  {
+    "revision": "3eb4134a53984d263def717da7e32018",
+    "url": "/jihnius/static/media/Poster_Club.3eb4134a.jpg"
+  },
+  {
+    "revision": "e019d86b9e3b55db0978c28b5c92d809",
+    "url": "/jihnius/static/media/Poster_Illuminated.e019d86b.jpg"
+  },
+  {
+    "revision": "4c605182f7d901eabafdeacf16fdc3d7",
+    "url": "/jihnius/static/media/Poster_Play.4c605182.jpg"
+  },
+  {
+    "revision": "72560ff4bc95fa3bec649311287ca05a",
+    "url": "/jihnius/static/media/Poster_Sugar.72560ff4.jpg"
+  },
+  {
+    "revision": "d1c7bf93ac67e412d688e65d2d377784",
+    "url": "/jihnius/static/media/Poster_Valentines.d1c7bf93.jpg"
+  },
+  {
+    "revision": "23b4f5ae917194b77b9907fab3f54d19",
+    "url": "/jihnius/static/media/Saycheese.23b4f5ae.jpg"
+  },
+  {
+    "revision": "d642a6c66e46cb47333f67b4cd5b611c",
+    "url": "/jihnius/static/media/homep.d642a6c6.png"
+  }
+]);
